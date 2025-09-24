2fafce9e29c476053a083d460c4ed428e9170817
KikuzCom1207 <231205075@ccmailg.meijo-u.ac.jp>
2025-09-24 14:07:56 +0900

    add ruff

--- source/conf.py
+++ source/conf.py
@@ -6,40 +6,40 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'my-lecture-memo'
-copyright = '2025, KikuzCom1207'
-author = 'KikuzCom1207'
+project = "my-lecture-memo"
+copyright = "2025, KikuzCom1207"
+author = "KikuzCom1207"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'sphinx_markdown_tables',
-    'sphinx_copybutton',
-    'sphinxcontrib.mermaid',
-    'sphinx_diagrams',
+    "myst_parser",
+    "sphinx_markdown_tables",
+    "sphinx_copybutton",
+    "sphinxcontrib.mermaid",
+    "sphinx_diagrams",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'ja'
+language = "ja"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 # ソースファイルの拡張子の設定
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
 # Master document
-master_doc = 'index'
+master_doc = "index"
 
 # MyST-Parserの設定
 myst_enable_extensions = [
@@ -52,13 +52,13 @@ myst_enable_extensions = [
 ]
 
 # sphinx_copybuttonの設定
-copybutton_prompt_text = '>>> '
+copybutton_prompt_text = ">>> "
 copybutton_prompt_is_regexp = True
 
 # sphinxcontrib-mermaidの設定
-mermaid_output_format = 'svg'
+mermaid_output_format = "svg"
 
 # カスタムCSSの設定
 html_css_files = [
-    'custom.css',
+    "custom.css",
 ]
